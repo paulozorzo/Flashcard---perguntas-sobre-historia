dafec57f7509f4b3521c27a392911abb6bd857db
paulozorzo <paulo.zorzo@escola.pr.gov.br>
2024-10-17 11:02:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Historia do Cristianismo',
+    'Quando ocorreu o Cisma do Oriente, e quem era o Papa de Roma durante esse período?',
+    'Ocorreu em 1054, e o Papa era Leão IX.'
+)
+
+criaCartao(
+    'Proclamação da República do Brasil (1889)',
+    'Qual era a posição dos ex donos de escravos durante a Proclamação da Republica?',
+    'Em grande maioria a favor da República, pois a monarquia havia abolido a escravidão.'
+)
+
+criaCartao(
+    'Movimentos politicos do século XX',
+    'Qual partido não participou da Conferência fascista de Montreux (1934): Guarda de Ferro (Romênia), Nacional Socialismo (Alemanha), FE de las JONS (Espanha) ou Army Comrades Association (Irlanda)?',
+    'O Nacional Socialismo, por não compartilhar dos mesmos ideais.'
+)
+
+criaCartao(
+    'Imigrações para o Brasil no século XIX',
+    'Qual dialeto de alemão (ou lingua) se originou dos imigrantes vindos da região sudoeste da alemanha, perto de Luxemburgo?',
+    'O idioma Hunsriqueano (ou Hunsriqueano rio-grandense), que deriva do dialeto Hunsrückisch.'
+)
+
+criaCartao(
+    'Movimento negro no Brasil',
+    'Quem foi o primeiro lider do primeiro movimento negro do Brasil, a Frente Negra Brasileira?',
+    'Foi Arlindo Veiga dos Santos, que também era monarquista.'
+)
+
+criaCartao(
+    'Movimentos do século XIX na Europa',
+    'Qual acontecimento desencadeou a chamada Primavera dos Povos de 1848?',
+    'A abdicação do rei Luiz Felipe I da França.'
+)
